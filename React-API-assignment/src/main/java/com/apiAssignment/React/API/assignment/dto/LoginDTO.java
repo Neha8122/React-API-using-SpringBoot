c14c150b9890a6d5a7610072f2df89a213b14efd
Neha8122 <90138504+Neha8122@users.noreply.github.com>
2023-07-07 21:57:30 +0530

    Add files via upload

---
+++ React-API-assignment/src/main/java/com/apiAssignment/React/API/assignment/dto/LoginDTO.java
@@ -0,0 +1,32 @@
+package com.apiAssignment.React.API.assignment.dto;
+
+public class LoginDTO {
+    private String username;
+    private String password;
+
+    // Constructors, getters, and setters
+
+    public LoginDTO() {
+    }
+
+    public LoginDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ React-API-assignment/src/main/java/com/apiAssignment/React/API/assignment/dto/UserDTO.java
@@ -0,0 +1,28 @@
+// UserDTO.java
+package com.apiAssignment.React.API.assignment.dto;
+
+public class UserDTO {
+    private String username;
+    private String password;
+    private String email;
+
+    // Getters
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Setters (if needed)
+
+    // ...
+}
+
+
+
